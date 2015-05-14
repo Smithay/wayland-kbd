4f1964f0665ae69433419844b8a14bfb5f416ff6
Victor Berger <victor.berger@m4x.org>
2015-05-14 20:39:59 +0200

    Version 0.1.0.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! Keyboard mapping utility for `wayland-client` using libxkbcommon.
+//!
+//! This library provides a simple wrapper for the wayland Keyboard objects,
+//! handling all keymap issues using libxkbcommon in a dynamic way (loading the
+//! library dynamically and thus not being linked to it).
+//!
+//! To use it, simply call `MappedKeyboard::new(..)` to wrap you keyboard object
+//! and set the key_action callback. This callback will be provided the keycode,
+//! the new state of the key (up or down), as well as a `KbState` handle.
+//!
+//! This handle will allow you to retrive the keysym associated to the keycode
+//! and compare it to the values defined in the `keysyms` module, or directly
+//! restrieve an (utf8) String representation of this character.
+
+
 #[macro_use] extern crate bitflags;
 #[macro_use] extern crate lazy_static;
 extern crate libc;
