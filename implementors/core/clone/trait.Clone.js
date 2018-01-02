c7810dc9a8e2cba99457e901b72c00f263a48137
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-02 19:59:40 +0000

    Deploy Smithay/wayland-kbd to github.com/Smithay/wayland-kbd.git:gh-pages

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wayland_kbd"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"wayland_kbd/struct.ModifiersState.html\" title=\"struct wayland_kbd::ModifiersState\">ModifiersState</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wayland_kbd"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"wayland_kbd/struct.ModifiersState.html\" title=\"struct wayland_kbd::ModifiersState\">ModifiersState</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"wayland_kbd/enum.MappedKeyboardError.html\" title=\"enum wayland_kbd::MappedKeyboardError\">MappedKeyboardError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
